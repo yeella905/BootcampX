311112ac2febb7a3eaaa808ddd6f6914f319d711
yeella905 <yeella905@gmail.com>
2025-02-21 20:19:06 -0500

    edit:restructuured the query so we can get cohorts name instead

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -20,12 +20,16 @@ const pool = new Pool({
 pool
   .query(
     `
-SELECT id, name, cohort_id
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
+JOIN cohorts ON cohorts.id = cohort_id
 LIMIT 5;
 `
   )
   .then((res) => {
-    console.log(res.rows);
-  })
-  .catch((err) => console.error("query error", err.stack));
+    res.rows.forEach((user) => {
+        console.log(
+          `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+        );
+      });
+    });
